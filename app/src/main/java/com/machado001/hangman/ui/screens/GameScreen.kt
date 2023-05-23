879d2c9ef472bdab0824addd9a462845ffb15bef
HaradaYasuo <academic.dev0531@gmail.com>
2023-05-23 18:20:06 -0300

    first commit

---
+++ app/src/main/java/com/machado001/hangman/ui/screens/GameScreen.kt
@@ -0,0 +1,11 @@
+package com.machado001.hangman.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GameScreen(){
+    Text(text = "GameScreen")
+}
+
+
---
+++ app/src/main/java/com/machado001/hangman/ui/screens/HomeScreen.kt
@@ -0,0 +1,72 @@
+package com.machado001.hangman.ui.screens
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.machado001.hangman.R
+import com.machado001.hangman.ui.theme.HangmanTheme
+
+@Composable
+fun Home(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize() ){
+        CenterDiv(modifier = modifier.align(Alignment.Center))
+
+    }
+}
+
+@Composable
+fun CenterDiv(modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "" )
+        Text(
+            text = stringResource(id = R.string.app_name).uppercase(),
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Black,
+        )
+        Row(modifier = modifier.padding(16.dp)){
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = modifier.padding(end = 16.dp),
+                elevation = ButtonDefaults.buttonElevation(16.dp)
+            ) {
+                Text(text = "Jogar")
+            }
+            OutlinedButton(
+                onClick = { /*TODO */ }
+            ) {
+                Text(text = "Configurações")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HangmanTheme {
+        Home()
+    }
+}
